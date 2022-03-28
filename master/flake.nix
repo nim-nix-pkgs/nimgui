2859acb27a7856bb55d5573e7b42450460caff9d
hugosenari <hugosenari@gmail.com>
2022-03-27 21:36:06 -0300

    chore: re index nimgui

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-nimgui-master.ref   = "refs/heads/master";
   inputs.src-nimgui-master.owner = "zacharycarter";
   inputs.src-nimgui-master.repo  = "nimgui";
+  inputs.src-nimgui-master.dir   = "";
   inputs.src-nimgui-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
